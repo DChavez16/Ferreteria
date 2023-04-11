a8efdb16bf8c5c3eee78c506f5b892eec3f6afff
Daniel <Daniel_ChavezT@outlook.com>
2023-04-10 18:52:33 -0600

    Project Structure

---
+++ src/main/kotlin/ui/mainContainer/MainContainer.kt
@@ -0,0 +1,13 @@
+package ui.mainContainer
+
+import controller.MainController
+import androidx.compose.runtime.Composable
+
+
+/**
+ * MainContainer main composable
+ */
+@Composable
+fun MainContainer() {
+    val mainController = MainController()
+}
