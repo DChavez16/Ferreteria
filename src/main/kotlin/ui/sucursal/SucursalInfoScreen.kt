eec2b4383a624c7965f1ba749be9f639a62dd8ea
DChavez16 <dchavez16@users.noreply.github.com>
2023-04-25 16:57:23 -0600

    - Implemented full functionality of Sucursal screens

--- src/main/kotlin/ui/sucursal/SucursalInfoScreen.kt
+++ src/main/kotlin/ui/sucursal/SucursalInfoScreen.kt
@@ -1,32 +1,148 @@
 package ui.sucursal
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import data.model.Sucursal
+import data.model.SucursalTestList
+import ui.util.BottomButtons
+import ui.util.ScreenHeader
 
 @Composable
 fun SucursalInfoScreen(
     editable: Boolean,
     onReturnButtonClick: () -> Unit,
-    onMainButtonClick: () -> Unit,
+    onMainButtonClick: (Sucursal) -> Unit,
     onDeleteButtonClick: () -> Unit = {},
     selectedSucursal: Sucursal? = null
 ) {
-    Box(contentAlignment = Alignment.Center) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Text(if(editable) "Editar" else "Agregar")
-            Text("Regresar", modifier = Modifier.clickable { onReturnButtonClick() })
+    Column(modifier = Modifier.fillMaxHeight()) {
+        // Screen header
+        ScreenHeader(
+            headerTitle = if (editable) "Editar sucursal" else " Agregar sucursal",
+            onReturnButtonClick = onReturnButtonClick
+        )
+
+        // Sucursal form
+        SucursalForm(
+            currentSucursal = selectedSucursal,
+            editable = editable,
+            onMainButtonClick = onMainButtonClick,
+            onDeleteButtonClick = onDeleteButtonClick,
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
+private fun SucursalForm(
+    currentSucursal: Sucursal?,
+    editable: Boolean,
+    onMainButtonClick: (Sucursal) -> Unit,
+    onDeleteButtonClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    // Form variables
+    var sucursalNombre by remember { mutableStateOf(currentSucursal?.name ?: "") }
+    var sucursalDireccion by remember { mutableStateOf(currentSucursal?.contacto?.direccion ?: "") }
+    var sucursalTelefono by remember { mutableStateOf(currentSucursal?.contacto?.telefono ?: "") }
+
+    Column(modifier = modifier.fillMaxHeight()) {
+        // Form body
+        SucursalFormContent(
+            nombre = sucursalNombre, onNombreValueChange = { sucursalNombre = it },
+            direccion = sucursalDireccion, onDireccionValueChange = { sucursalDireccion = it },
+            telefono = sucursalTelefono, onTelefonoValueChange = { sucursalTelefono = it },
+            modifier = Modifier.weight(1f)
+        )
+
+        // Bottom button sto interact with the screen
+        Row(modifier = Modifier.fillMaxWidth()) {
+            if (editable) {
+                BottomButtons(
+                    twoButtons = false,
+                    firstButtonText = "Borrar",
+                    firstButtonAction = onDeleteButtonClick,
+                    modifier = Modifier.weight(1f)
+                )
+            }
+            BottomButtons(
+                twoButtons = true,
+                firstButtonText = if (editable) "Actualizar" else "Agregar",
+                firstButtonAction = { onMainButtonClick(SucursalTestList[0]) },
+                secondButtonText = "Limpiar campos",
+                secondButtonAction = {
+                    sucursalNombre = ""
+                    sucursalDireccion = ""
+                    sucursalTelefono = ""
+                },
+                firstButtonEnabled = validateCorrectFields(
+                    sucursalNombre, sucursalDireccion, sucursalTelefono
+                ),
+                modifier = Modifier.weight(1f)
+            )
         }
     }
 }
+
+@Composable
+private fun SucursalFormContent(
+    nombre: String,
+    onNombreValueChange: (String) -> Unit,
+    direccion: String,
+    onDireccionValueChange: (String) -> Unit,
+    telefono: String,
+    onTelefonoValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(horizontalArrangement = Arrangement.Center, modifier = modifier.fillMaxWidth()) {
+        Column(verticalArrangement = Arrangement.Center, modifier = Modifier.fillMaxHeight().fillMaxWidth(0.4f)) {
+            // Nombre form field
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(bottom = 8.dp)
+            ) {
+                Text(text = "Nombre:", style = MaterialTheme.typography.h6, modifier = Modifier.weight(1f))
+                OutlinedTextField(
+                    value = nombre, onValueChange = onNombreValueChange, singleLine = true, modifier = Modifier.weight(2f)
+                )
+            }
+
+            // Direccion form field
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(bottom = 8.dp)
+            ) {
+                Text(text = "Direccion:", style = MaterialTheme.typography.h6, modifier = Modifier.weight(1f))
+                OutlinedTextField(
+                    value = direccion, onValueChange = onDireccionValueChange, singleLine = true, modifier = Modifier.weight(2f)
+                )
+            }
+
+            // Correo form field
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "Telefono:", style = MaterialTheme.typography.h6, modifier = Modifier.weight(1f))
+                OutlinedTextField(
+                    value = telefono, onValueChange = onTelefonoValueChange, singleLine = true, modifier = Modifier.weight(2f)
+                )
+            }
+        }
+    }
+}
+
+
+/*
+Helper methods
+*/
+private fun validateCorrectFields(nombre: String, direccion: String, telefono: String) =
+    nombre.isNotEmpty() && direccion.isNotEmpty() && telefono.isNotEmpty()
--- src/main/kotlin/ui/sucursal/SucursalList.kt
+++ src/main/kotlin/ui/sucursal/SucursalList.kt
@@ -1,16 +1,24 @@
 package ui.sucursal
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.ScrollbarAdapter
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import data.model.Sucursal
-import data.model.SucursalTestList
+import ui.util.BottomButtons
 
 @Composable
 fun SucursalList(
@@ -18,15 +26,120 @@ fun SucursalList(
     onAddSucursalClick: () -> Unit,
     onEditSucursalClick: (Sucursal) -> Unit
 ) {
-    Box(contentAlignment = Alignment.Center) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxSize()
+    Column(modifier = Modifier.fillMaxSize().padding(32.dp)) {
+        // Sucursal list surface
+        Surface(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier.fillMaxSize().border(width = Dp.Hairline, color = Color.Gray)) {
+                SucursalListHeader()
+                SucursalListContent(
+                    sucursalList = sucursalList,
+                    onEditSucursalClick = onEditSucursalClick
+                )
+            }
+        }
+
+        // Bottom buttons
+        BottomButtons(
+            twoButtons = false,
+            firstButtonText = "Agregar sucursal",
+            firstButtonAction = onAddSucursalClick
+        )
+    }
+}
+
+@Composable
+private fun SucursalListHeader() {
+    Surface(elevation = 8.dp) {
+        Row(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+            Text(
+                text = "id",
+                modifier = Modifier.weight(1f),
+                style = MaterialTheme.typography.h6,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = "Nombre",
+                modifier = Modifier.weight(2f),
+                style = MaterialTheme.typography.h6,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = "Direccion",
+                modifier = Modifier.weight(2.5f),
+                style = MaterialTheme.typography.h6,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = "Telefono",
+                modifier = Modifier.weight(1f),
+                style = MaterialTheme.typography.h6,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.weight(0.5f))
+        }
+    }
+}
+
+@Composable
+private fun SucursalListContent(
+    sucursalList: List<Sucursal>,
+    onEditSucursalClick: (Sucursal) -> Unit
+) {
+    Box {
+        val state = rememberLazyListState()
+
+        LazyColumn(state = state) {
+            items(sucursalList) {
+                SucursalListItem(it, onEditSucursalClick)
+                Divider(color = Color.Gray, thickness = Dp.Hairline)
+            }
+        }
+
+        VerticalScrollbar(
+            modifier = Modifier.align(Alignment.CenterEnd),
+            adapter = ScrollbarAdapter(scrollState = state)
+        )
+    }
+}
+
+@Composable
+private fun SucursalListItem(
+    sucursal: Sucursal,
+    onEditSucursalClick: (Sucursal) -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(
+            text = "${sucursal.id}",
+            modifier = Modifier.weight(1f),
+            style = MaterialTheme.typography.body1,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = sucursal.name,
+            modifier = Modifier.weight(2f),
+            style = MaterialTheme.typography.body1,
+            textAlign = TextAlign.Start
+        )
+        Text(
+            text = sucursal.contacto.direccion,
+            modifier = Modifier.weight(2.5f),
+            style = MaterialTheme.typography.body1,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = sucursal.contacto.telefono,
+            modifier = Modifier.weight(1f),
+            style = MaterialTheme.typography.body1,
+            textAlign = TextAlign.Center
+        )
+        IconButton(
+            modifier = Modifier.weight(0.5f).height(20.dp),
+            onClick = { onEditSucursalClick(sucursal) }
         ) {
-            Text("Lista sucursal")
-            Text("Agregar", modifier = Modifier.clickable { onAddSucursalClick() })
-            Text("Editar", modifier = Modifier.clickable { onEditSucursalClick(SucursalTestList[0]) })
+            Icon(imageVector = Icons.Default.Edit, contentDescription = null)
         }
     }
 }
